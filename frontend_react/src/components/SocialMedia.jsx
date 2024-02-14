f75fd4d68f51280d11f1e58db47e41bb56ce9efd
Meagan Townsend <meagantownsend337@gmail.com>
2024-02-14 19:22:37 -0400

    added social links to profiles

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -4,18 +4,37 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 
 const SocialMedia = () => {
-  return( 
+  return (
     <div className="app__social">
-        <div>
-            <BsGithub/>
-        </div>
-        <div>
-            <FaLinkedinIn/>
-        </div>
-        <div>
-            <BsInstagram/>
-        </div>
-    </div>)
+      <div>
+        <a
+          href="https://github.com/megs-townie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/meagan-townsend/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedinIn />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.instagram.com/runtime_tamer/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsInstagram />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default SocialMedia;
